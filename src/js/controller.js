7c169dc6b905da8d3c2019dba66fac7c132c2d7f
flamenow <marcosmujica3@gmail.com>
2022-05-04 11:47:49 -0300

    Initial commit

---
+++ src/js/controller.js
@@ -0,0 +1,74 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime/runtime';
+// if (module.hot) {
+//   module.hot.accept();
+// }
+const recipeContainer = document.querySelector('.recipe');
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+    //1) loading recipe
+    await model.loadRecipe(id);
+
+    //2) rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // 1- get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2 - load search resultas
+    await model.loadSearchResults(query);
+
+    // 3 - render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4 - render pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  //  update the recipe srvings (in state)
+  model.updateServings(newServings);
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
